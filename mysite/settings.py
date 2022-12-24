e29090deeae3763faa9074109bf51efc3f3247bc
Adnan <adnankhurshid251@gmail.com>
2022-12-24 20:26:36 +0530

    added vercel config

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,4 +136,4 @@ MEDIA_URL = '/files/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
